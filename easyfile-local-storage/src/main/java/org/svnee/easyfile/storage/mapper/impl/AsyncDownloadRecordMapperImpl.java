3bf9d0b22000234f88a40ddeba209ee78c5bf9db
svnee <svnee>
2022-04-23 12:15:15 +0800

    feats(初始化项目): local recordMapper 支持

--- easyfile-local-storage/src/main/java/org/svnee/easyfile/storage/mapper/impl/AsyncDownloadRecordMapperImpl.java
+++ easyfile-local-storage/src/main/java/org/svnee/easyfile/storage/mapper/impl/AsyncDownloadRecordMapperImpl.java
@@ -1,9 +1,17 @@
 package org.svnee.easyfile.storage.mapper.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.svnee.easyfile.common.dictionary.UploadStatusEnum;
+import org.svnee.easyfile.common.util.CollectionUtils;
+import org.svnee.easyfile.common.util.StringUtils;
 import org.svnee.easyfile.storage.entity.AsyncDownloadRecord;
 import org.svnee.easyfile.storage.mapper.AsyncDownloadRecordMapper;
 import org.svnee.easyfile.storage.mapper.condition.UploadInfoChangeCondition;
@@ -18,28 +26,124 @@ public class AsyncDownloadRecordMapperImpl implements AsyncDownloadRecordMapper 
     private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public int insertSelective(AsyncDownloadRecord asyncDownloadRecord) {
-        return 0;
+    public int insertSelective(AsyncDownloadRecord downloadRecord) {
+        final String sql =
+            "insert into ef_async_download_record (download_task_id, app_id,download_code,upload_status, file_url, file_system, download_operate_by,"
+                + "download_operate_name, remark, notify_enable_status,notify_email, max_server_retry, current_retry,download_num,last_execute_time,"
+                + "invalid_time,execute_param,error_msg, version, create_time, update_time, create_by, update_by) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        return jdbcTemplate.update(sql, downloadRecord.getDownloadTaskId(),
+            downloadRecord.getAppId(),
+            downloadRecord.getDownloadCode(),
+            downloadRecord.getUploadStatus(),
+            downloadRecord.getFileUrl(),
+            downloadRecord.getFileSystem(),
+            downloadRecord.getDownloadOperateBy(),
+            downloadRecord.getDownloadOperateName(),
+            downloadRecord.getRemark(),
+            downloadRecord.getNotifyEnableStatus(),
+            downloadRecord.getNotifyEmail(),
+            downloadRecord.getMaxServerRetry(),
+            downloadRecord.getCurrentRetry(),
+            downloadRecord.getDownloadNum(),
+            downloadRecord.getLastExecuteTime(),
+            downloadRecord.getInvalidTime(),
+            downloadRecord.getExecuteParam(),
+            downloadRecord.getErrorMsg(),
+            downloadRecord.getVersion(),
+            downloadRecord.getCreateTime(),
+            downloadRecord.getUpdateTime(),
+            downloadRecord.getCreateBy(),
+            downloadRecord.getUpdateBy());
     }
 
     @Override
     public AsyncDownloadRecord findById(Long id) {
+        final String sql =
+            "select id,download_task_id, app_id,download_code,upload_status, file_url, file_system, download_operate_by,"
+                + "download_operate_name, remark, notify_enable_status,notify_email, max_server_retry, current_retry,download_num,last_execute_time,"
+                + "invalid_time,execute_param,error_msg, version, create_time, update_time, create_by, update_by from ef_async_download_record where id = ?";
+        List<AsyncDownloadRecord> downloadRecordList = jdbcTemplate
+            .query(sql, new Object[]{id}, new AsyncDownloadRecordRowMapper());
+        if (CollectionUtils.isNotEmpty(downloadRecordList)) {
+            return downloadRecordList.get(0);
+        }
         return null;
     }
 
     @Override
     public int changeUploadInfo(UploadInfoChangeCondition condition) {
-        return 0;
+        List<Object> params = new ArrayList<>();
+        final StringBuilder sql = new StringBuilder(
+            "update ef_async_download_record set version=version+1,upload_status = ? ");
+        params.add(condition.getUploadStatus().getCode());
+        if (StringUtils.isNotBlank(condition.getFileUrl())) {
+            sql.append(",file_url = ?");
+            params.add(condition.getFileUrl());
+        }
+        if (StringUtils.isNotBlank(condition.getFileSystem())) {
+            sql.append(",file_system = ?");
+            params.add(condition.getFileSystem());
+        }
+        if (StringUtils.isNotBlank(condition.getErrorMsg())) {
+            sql.append(",error_msg = ?");
+            params.add(condition.getErrorMsg());
+        }
+        if (Objects.nonNull(condition.getLastExecuteTime())) {
+            sql.append(",last_execute_time = ?");
+            params.add(condition.getLastExecuteTime());
+        }
+        if (Objects.nonNull(condition.getInvalidTime())) {
+            sql.append(",invalid_time = ?");
+            params.add(condition.getInvalidTime());
+        }
+        sql.append(" where id = ?");
+        params.add(condition.getId());
+        return jdbcTemplate.update(sql.toString(), params.toArray());
     }
 
     @Override
     public int refreshUploadStatus(Long id, UploadStatusEnum oriUploadStatus, UploadStatusEnum tagUploadStatus,
         String updateBy) {
-        return 0;
+        final String sql = "update ef_async_download_record set upload_status = ?,update_by = ? where id = ? AND upload_status = ?";
+        return jdbcTemplate.update(sql, tagUploadStatus.getCode(), updateBy, id, oriUploadStatus.getCode());
     }
 
     @Override
     public void download(Long id, UploadStatusEnum uploadStatus) {
+        final String sql = "update ef_async_download_record set download_num = download_num + 1 where id = ? and upload_status = ?";
+        jdbcTemplate.update(sql, id, uploadStatus.getCode());
+    }
 
+    private static class AsyncDownloadRecordRowMapper implements RowMapper<AsyncDownloadRecord> {
+
+        @Override
+        public AsyncDownloadRecord mapRow(ResultSet rs, int rowNum) throws SQLException {
+            AsyncDownloadRecord downloadRecord = new AsyncDownloadRecord();
+            downloadRecord.setId(rs.getLong("id"));
+            downloadRecord.setDownloadTaskId(rs.getLong("download_task_id"));
+            downloadRecord.setAppId(rs.getString("app_id"));
+            downloadRecord.setDownloadCode(rs.getString("download_code"));
+            downloadRecord.setUploadStatus(UploadStatusEnum.fromCode(rs.getString("upload_status")));
+            downloadRecord.setFileUrl(rs.getString("file_url"));
+            downloadRecord.setFileSystem(rs.getString("file_system"));
+            downloadRecord.setDownloadOperateBy(rs.getString("download_operate_by"));
+            downloadRecord.setDownloadOperateName(rs.getString("download_operate_name"));
+            downloadRecord.setRemark(rs.getString("remark"));
+            downloadRecord.setNotifyEnableStatus(rs.getInt("notify_enable_status"));
+            downloadRecord.setNotifyEmail(rs.getString("notify_email"));
+            downloadRecord.setMaxServerRetry(rs.getInt("max_server_retry"));
+            downloadRecord.setCurrentRetry(rs.getInt("current_retry"));
+            downloadRecord.setExecuteParam(rs.getString("execute_param"));
+            downloadRecord.setErrorMsg(rs.getString("error_msg"));
+            downloadRecord.setLastExecuteTime(rs.getDate("last_execute_time"));
+            downloadRecord.setInvalidTime(rs.getDate("invalid_time"));
+            downloadRecord.setDownloadNum(rs.getInt("download_num"));
+            downloadRecord.setVersion(rs.getInt("version"));
+            downloadRecord.setCreateTime(rs.getDate("create_time"));
+            downloadRecord.setUpdateTime(rs.getDate("update_time"));
+            downloadRecord.setCreateBy(rs.getString("create_by"));
+            downloadRecord.setUpdateBy(rs.getString("update_by"));
+            return downloadRecord;
+        }
     }
 }
--- easyfile-local-storage/src/main/java/org/svnee/easyfile/storage/mapper/impl/AsyncDownloadTaskMapperImpl.java
+++ easyfile-local-storage/src/main/java/org/svnee/easyfile/storage/mapper/impl/AsyncDownloadTaskMapperImpl.java
@@ -1,10 +1,17 @@
 package org.svnee.easyfile.storage.mapper.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.svnee.easyfile.common.util.CollectionUtils;
+import org.svnee.easyfile.common.util.MapUtils;
 import org.svnee.easyfile.storage.entity.AsyncDownloadTask;
 import org.svnee.easyfile.storage.mapper.AsyncDownloadTaskMapper;
 
@@ -18,22 +25,69 @@ public class AsyncDownloadTaskMapperImpl implements AsyncDownloadTaskMapper {
     private final JdbcTemplate jdbcTemplate;
 
     @Override
-    public int insertSelective(AsyncDownloadTask asyncDownloadTask) {
-        return 0;
+    public int insertSelective(AsyncDownloadTask task) {
+        final String sql =
+            " insert into ef_async_download_task(task_code, task_desc, limiting_strategy, app_id,unified_app_id, enable_status, version, create_time, update_time,"
+                + "create_by, update_by, is_deleted) values (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        return jdbcTemplate
+            .update(sql, task.getTaskCode(), task.getTaskDesc(),
+                task.getLimitingStrategy(), task.getAppId(),
+                task.getUnifiedAppId(), task.getEnableStatus(),
+                task.getVersion(), task.getCreateTime(), task.getUpdateTime(),
+                task.getCreateBy(), task.getUpdateBy(), task.getIsDeleted());
     }
 
     @Override
     public AsyncDownloadTask selectByDownloadCode(String taskCode, String appId) {
-        return null;
+        final String sql =
+            "select id, task_code, task_desc, limiting_strategy, app_id,unified_app_id, enable_status, version, create_time, update_time,\n"
+                + "create_by, update_by, is_deleted from ef_async_download_task where task_code=? and app_id=?";
+        List<AsyncDownloadTask> taskList = jdbcTemplate
+            .query(sql, new Object[]{taskCode, appId}, new AsyncDownloadTaskRowMapper());
+        if (CollectionUtils.isEmpty(taskList)) {
+            return null;
+        }
+        return taskList.get(0);
     }
 
     @Override
     public List<AsyncDownloadTask> listByDownloadCode(List<String> downloadCodeList, List<String> appIdList) {
-        return Collections.emptyList();
+        final String sql =
+            "select id, task_code, task_desc, limiting_strategy, app_id,unified_app_id, enable_status, version, create_time, update_time,\n"
+                + "create_by, update_by, is_deleted from ef_async_download_task where task_code in (:taskCodeList) and app_id in (:appIdList)";
+
+        Map<String, Object> paramMap = MapUtils.newHashMapWithExpectedSize(2);
+        paramMap.put("taskCodeList", downloadCodeList);
+        paramMap.put("appIdList", appIdList);
+
+        return new NamedParameterJdbcTemplate(jdbcTemplate).query(sql, paramMap, new AsyncDownloadTaskRowMapper());
     }
 
     @Override
     public int refreshTaskDesc(Long id, String taskDesc) {
-        return 0;
+        final String sql = "update ef_async_download_task set task_desc = ? where id = ?";
+        return jdbcTemplate.update(sql, taskDesc, id);
+    }
+
+    public static class AsyncDownloadTaskRowMapper implements RowMapper<AsyncDownloadTask> {
+
+        @Override
+        public AsyncDownloadTask mapRow(ResultSet resultSet, int i) throws SQLException {
+            AsyncDownloadTask asyncDownloadTask = new AsyncDownloadTask();
+            asyncDownloadTask.setId(resultSet.getLong("id"));
+            asyncDownloadTask.setTaskCode(resultSet.getString("task_code"));
+            asyncDownloadTask.setTaskDesc(resultSet.getString("task_desc"));
+            asyncDownloadTask.setAppId(resultSet.getString("app_id"));
+            asyncDownloadTask.setUnifiedAppId(resultSet.getString("unified_app_id"));
+            asyncDownloadTask.setEnableStatus(resultSet.getInt("enable_status"));
+            asyncDownloadTask.setLimitingStrategy(resultSet.getString("limiting_strategy"));
+            asyncDownloadTask.setVersion(resultSet.getInt("version"));
+            asyncDownloadTask.setCreateTime(resultSet.getDate("create_time"));
+            asyncDownloadTask.setUpdateTime(resultSet.getDate("update_time"));
+            asyncDownloadTask.setCreateBy(resultSet.getString("create_by"));
+            asyncDownloadTask.setUpdateBy(resultSet.getString("update_by"));
+            asyncDownloadTask.setIsDeleted(resultSet.getLong("is_deleted"));
+            return asyncDownloadTask;
+        }
     }
 }
