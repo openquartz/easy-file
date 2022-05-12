7497732ee2d444f4e7ee168a8291bafe7151a22f
svnee <svnee>
2022-05-12 19:46:01 +0800

    refactor(缓存支持): 优化代码

--- easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/exception/DownloadErrorCode.java
+++ easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/exception/DownloadErrorCode.java
@@ -2,6 +2,7 @@ package org.svnee.easyfile.starter.exception;
 
 import lombok.Getter;
 import org.svnee.easyfile.common.exception.EasyFileErrorCode;
+import org.svnee.easyfile.starter.executor.BaseDownloadExecutor;
 
 /**
  * 下载异常码
@@ -19,18 +20,27 @@ public enum DownloadErrorCode implements EasyFileErrorCode {
     /*处理文件下载异常*/
     HANDLE_DOWNLOAD_FILE_ERROR("04", "处理下载文件异常"),
     DOWNLOAD_FILE_NAME_ENCODING_ERROR("05", "下载文件名Encoding异常"),
+    BASE_DOWNLOAD_EXECUTOR_IMPL_ILL_ERROR("06",
+        "class:{0} must implement " + BaseDownloadExecutor.class, true),
+    DOWNLOAD_CODE_NOT_UNIQ_ERROR("07", "downloadCode:({0}) not unique,please check it!", true),
 
     ;
     /** 异常码 */
     private final String errorCode;
     /** 异常信息 */
     private final String errorMsg;
+    private final boolean replacePlaceHold;
 
     /** 基础码前缀 */
     private static final String SIMPLE_BASE_PREFIX = "Download-";
 
     DownloadErrorCode(String errorCode, String errorMsg) {
+        this(errorCode, errorMsg, false);
+    }
+
+    DownloadErrorCode(String errorCode, String errorMsg, boolean replacePlaceHold) {
         this.errorCode = SIMPLE_BASE_PREFIX + errorCode;
         this.errorMsg = errorMsg;
+        this.replacePlaceHold = replacePlaceHold;
     }
 }
--- easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/processor/FileExportExecutorPostProcessor.java
+++ easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/processor/FileExportExecutorPostProcessor.java
@@ -3,7 +3,9 @@ package org.svnee.easyfile.starter.processor;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.svnee.easyfile.common.annotation.FileExportExecutor;
+import org.svnee.easyfile.common.exception.EasyFileException;
 import org.svnee.easyfile.common.util.SpringContextUtil;
+import org.svnee.easyfile.starter.exception.DownloadErrorCode;
 import org.svnee.easyfile.starter.executor.BaseDownloadExecutor;
 
 /**
@@ -20,13 +22,14 @@ public class FileExportExecutorPostProcessor implements BeanPostProcessor {
 
         if (clazz.isAnnotationPresent(FileExportExecutor.class)) {
             if (!BaseDownloadExecutor.class.isAssignableFrom(clazz)) {
-                throw new RuntimeException(String.format("%s must implement %s", clazz, BaseDownloadExecutor.class));
+                throw EasyFileException.replacePlaceHold(DownloadErrorCode.BASE_DOWNLOAD_EXECUTOR_IMPL_ILL_ERROR,
+                    BaseDownloadExecutor.class);
             }
             FileExportExecutor exportExecutor = clazz.getAnnotation(FileExportExecutor.class);
             if (FileExportExecutorSupport.contains(exportExecutor.value())
                 && !clazz.equals(FileExportExecutorSupport.get(exportExecutor.value()).getClass())) {
-                throw new RuntimeException(
-                    String.format("downloadCode:(%s) not unique,please check it!", exportExecutor.value()));
+                throw EasyFileException
+                    .replacePlaceHold(DownloadErrorCode.DOWNLOAD_CODE_NOT_UNIQ_ERROR, exportExecutor.value());
             }
             FileExportExecutorSupport.register(exportExecutor.value(), exportExecutor, (BaseDownloadExecutor) bean);
         }
