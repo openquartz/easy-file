4c9f7334292490fd8fe07533d306950c9ed31e50
svnee <svnee>
2022-05-14 22:46:27 +0800

    feats(缓存支持): add test

---
+++ easyfile-spring-boot-starter/src/test/java/org/svnee/SpelTest.java
@@ -0,0 +1,33 @@
+package org.svnee;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.expression.Expression;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.svnee.easyfile.common.response.ExportResult;
+
+/**
+ * @author xuzhao
+ **/
+public class SpelTest {
+
+    public static void main(String[] args) {
+
+        Map<String, Object> paramMap = new HashMap<>();
+
+        ExportResult exportResult = new ExportResult();
+        exportResult.setRegisterId(100L);
+        paramMap.put("cache", exportResult);
+
+        StandardEvaluationContext tarEvaluationContext = new StandardEvaluationContext();
+        tarEvaluationContext.setVariables(paramMap);
+
+        SpelExpressionParser expressionParser = new SpelExpressionParser();
+        Expression expression = expressionParser.parseExpression("#cache.registerId");
+        Object oriValue = expression.getValue(tarEvaluationContext);
+        System.out.println(oriValue);
+    }
+
+
+}
