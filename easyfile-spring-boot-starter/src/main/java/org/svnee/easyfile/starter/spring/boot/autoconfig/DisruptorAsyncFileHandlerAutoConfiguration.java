595b17246e3d99263df79911eb91fe260be4f8be
svnee <svnee>
2022-09-03 15:24:51 +0800

    feat(EasyFile): clean code

--- easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/spring/boot/autoconfig/DisruptorAsyncFileHandlerAutoConfiguration.java
+++ easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/spring/boot/autoconfig/DisruptorAsyncFileHandlerAutoConfiguration.java
@@ -2,7 +2,6 @@ package org.svnee.easyfile.starter.spring.boot.autoconfig;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
