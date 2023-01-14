8ea098a5dd5e7c349cadab0a333d2444fdd5b6e3
svnee <svnee>
2023-01-14 11:04:22 +0800

    refactor(EasyFile): test

---
+++ easyfile-example/easyfile-example-local/src/main/java/org/svnee/easyfile/example/interceptor/WebRequestInterceptor.java
@@ -0,0 +1,23 @@
+package org.svnee.easyfile.example.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+public class WebRequestInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info("[WebRequestInterceptor#preHandle]handler:{}", handler);
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+        @Nullable Exception ex) {
+        log.info("[WebRequestInterceptor#afterCompletion]handler:{}", handler);
+    }
+}
