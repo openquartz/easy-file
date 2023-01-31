6a8af8642fde866ce05e08e9a1ef1c4ae24ec0d8
svnee <svnee>
2023-01-31 22:29:57 +0800

    feat(EasyFile): add doc

--- easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/intercept/DownloadExecutorInterceptor.java
+++ easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/intercept/DownloadExecutorInterceptor.java
@@ -22,22 +22,24 @@ public interface DownloadExecutorInterceptor {
 
     /**
      * 执行之前处理
+     * 执行顺序: 按照{@link #order()} 从小到大 顺序执行
      *
      * @param executor 执行器
      * @param context 上下文
      * @param registerId 注册下载任务ID
-     * @param interceptorContext 拦截上下文
+     * @param interceptorContext 拦截上下文. 用于拦截时之前前后的上下文传递。
      */
     void beforeExecute(BaseDownloadExecutor executor, BaseDownloaderRequestContext context, Long registerId,
         InterceptorContext interceptorContext);
 
     /**
      * 执行之后处理
+     * 执行顺序: 按照{@link #order()} 从大到小 倒序执行
      *
      * @param executor 执行器
      * @param context 上下文
      * @param result 导出结果
-     * @param interceptorContext 拦截上下文
+     * @param interceptorContext 拦截上下文.用于拦截时之前前后的上下文传递。
      */
     void afterExecute(BaseDownloadExecutor executor, BaseDownloaderRequestContext context, ExportResult result,
         InterceptorContext interceptorContext);
