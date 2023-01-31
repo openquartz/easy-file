860953985b0a0d7e58b1b5ec028ee6650559cf7e
svnee <svnee>
2023-01-31 22:37:25 +0800

    feat(EasyFile): add doc

--- easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/executor/ExecuteProcessProbe.java
+++ easyfile-spring-boot-starter/src/main/java/org/svnee/easyfile/starter/executor/ExecuteProcessProbe.java
@@ -2,10 +2,13 @@ package org.svnee.easyfile.starter.executor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.NamedThreadLocal;
+import org.svnee.easyfile.common.constants.Constants;
 import org.svnee.easyfile.starter.executor.process.ExecuteProcessReporter;
 
 /**
  * 执行进度探针
+ * 用于上报 异步下载时的进度。
+ * 上报需要注意：由于使用{@link ThreadLocal} 做上下文上报器的传递。所以上报要求必须在执行器当前的主线程中调用{@link #report(Integer)}上报执行。
  *
  * @author svnee
  **/
@@ -43,11 +46,14 @@ public final class ExecuteProcessProbe {
     /**
      * 上报执行进度
      *
-     * @param executeProcess 执行进度
+     * @param executeProcess 执行进度 数据范围(0~100)
      */
     public static void report(Integer executeProcess) {
         if (executeProcess <= 0 || CURRENT_REPORTER.get() == null) {
             return;
+        }
+        if (executeProcess > Constants.FULL_PROCESS) {
+            executeProcess = Constants.FULL_PROCESS;
         }
         CURRENT_REPORTER.get().report(executeProcess);
     }
