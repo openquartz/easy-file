fb72253972fac3fdd64338d45a834cf71691b12d
svnee <svnee>
2023-03-04 21:59:36 +0800

    feat(EasyFile): redis-server

---
+++ easyfile-server/src/main/java/org/svnee/easyfile/server/config/RedissonConfig.java
@@ -0,0 +1,29 @@
+package org.svnee.easyfile.server.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author svnee
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private String port;
+
+    @Bean
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://"+host+":"+port+"");
+
+        return Redisson.create(config);
+    }
+}
